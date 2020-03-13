c02fa578986f28cffc18216678046d4498bcfe70
aldiemhaaa <aldi.emha@gmail.com>
2020-03-13 11:27:40 +0700

    clean code & fix sistem

--- api/views.py
+++ api/views.py
@@ -1,194 +1,105 @@
 from django.shortcuts import render
-import requests
 from datetime import date
-import hmac,hashlib
-import base64 
-import time
-import tempfile 
-import json
-import random
+import requests,hmac,hashlib,base64,time, tempfile , json ,random
 
-diag = "Data Peserta Kosong"
-hasil = ""
-nokar = "Data Peserta Kosong(2)"
-msg = ""
-noRujukan = ""
-fas = ""
-ppkPelayanan = ""
-poliRujukan = ""
-pelayanan = ""
-kelasRawat = ""
-comment = ""
-kodeSpesialisRujukan = ""
-dpjp = ""
+diag, hasil, nokar, msg, noRujukan, fas, ppkPelayanan, poliRujukan, pelayanan, kelasRawat, comment, kodeSpesialisRujukan, dpjp = "             "
 dateNow = str(date.today())
+
+# Header API BPJS
+def generateHeader():
+    consID = '27952'
+    # secretKey = 'rsm32h1'
+    stamp = str(
+            round(
+                time.time()
+            )
+            )
+    data = consID + '&' + stamp
+    resultdata = data.encode("utf-8")
+    signature = hmac.new(b"rsm32h1", resultdata, digestmod=hashlib.sha256).digest()
+    encodesignature = base64.b64encode(signature).decode()
+    return consID,stamp,encodesignature
+
+def getApi(endpoint):
+    # url = 'httpsL//new-api.bpjs-kesehatan.go.id:8080/new-vclaim-rest/'+endpoint+diagnosa
+    headers = {
+        "Accept":"application/json", 
+        "X-cons-id":generateHeader()[0],
+        "X-timestamp":generateHeader()[1],
+        "X-signature":generateHeader()[2]
+    }
+    response = requests.get(endpoint,headers = headers)
+    diag = response.json()
+    return diag
+    
+def postApi(endpoint,dataKey):
+    headers = {
+        "Accept":"application/json", 
+        "X-cons-id":generateHeader()[0],
+        "X-timestamp":generateHeader()[1],
+        "X-signature":generateHeader()[2]
+    }
+    response = requests.post(endpoint,data = dataKey, headers = headers)
+    hasil = response.json()
+    return hasil
+
 def index(request):
     if 'diagnosa' in request.POST:
-        consID = '27952'
-        # secretKey = 'rsm32h1'
-        stamp = str(
-                round(
-                    time.time()
-                )
-                )
-        data = consID + '&' + stamp
-        resultdata = data.encode("utf-8")
-        signature = hmac.new(b"rsm32h1", resultdata, digestmod=hashlib.sha256).digest()
-        encodesignature = base64.b64encode(signature).decode()
         try:
             diagnosa = request.POST['diagnosa']
-            global diag
-            global noRujukan
-            global poliRujukan
-            global pelayanan
-            global kelasRawat
-            global kodeSpesialisRujukan
-            noRujukan = diagnosa
             url = 'https://new-api.bpjs-kesehatan.go.id:8080/new-vclaim-rest/Rujukan/RS/%s' % diagnosa 
-            headers = {
-                "Accept":"application/json", 
-                "X-cons-id":consID,
-                "X-timestamp":stamp,
-                "X-signature":encodesignature
-            }
+            global diag, noRujukan, poliRujukan, pelayanan, kelasRawat, kodeSpesialisRujukan
+            noRujukan = diagnosa
+            diag = getApi(url)
 
-            response = requests.get(url,headers = headers)
-            diag = response.json()
-
+            # listNumber = open('list_number.txt',"w+")
             # for i in range(100):
             #     pin = ''.join(random.choice('0123456789') for _ in range(6))
-            #     print(i)
+            #     listNumber.write(pin)
+            # listNumber.close()
+
             
-            print(diag)
             # print(pin + " ini random numbernya")
 
             poliRujukan = diag['response']['rujukan']['provPerujuk']['kode']
             pelayanan = diag['response']['rujukan']['pelayanan']['kode']
             kelasRawat = diag['response']['rujukan']['peserta']['hakKelas']['kode']
             kodeSpesialisRujukan = diag['response']['rujukan']['poliRujukan']['kode']
-            # print(kodeSpesialisRujukan)
-            # print(kelasRawat)
-            # print(pelayanan)
-
-            # print(consID)
-            print(stamp)
-            print(encodesignature)
         except:
             return "none"
 
     elif 'nomorKartu' in request.POST:
-        consID = '27952'
-        # secretKey = 'rsm32h1'
-        stamp = str(
-                round(
-                    time.time()
-                )
-                )
-        data = consID + '&' + stamp
-        resultdata = data.encode("utf-8")
-        signature = hmac.new(b"rsm32h1", resultdata, digestmod=hashlib.sha256).digest()
-        encodesignature = base64.b64encode(signature).decode()
-        nokartu = request.POST['nomorKartu']
         global nokar
+        nokartu = request.POST['nomorKartu']
         url = 'https://new-api.bpjs-kesehatan.go.id:8080/new-vclaim-rest/Rujukan/RS/Peserta/%s' % nokartu
-        headers = {
-            "Accept":"application/json", 
-            "X-cons-id":consID,
-            "X-timestamp":stamp,
-            "X-signature":encodesignature
-        }
-
-        response = requests.get(url,headers = headers)
-        nokar = response.json()
+        
+        nokar = getApi(url)
         print(nokar)
 
     elif 'faskes' in request.POST:
-
-        consID = '27952'
-        # secretKey = 'rsm32h1'
-        stamp = str(
-                round(
-                    time.time()
-                )
-                )
-        data = consID + '&' + stamp
-        resultdata = data.encode("utf-8")
-        signature = hmac.new(b"rsm32h1", resultdata, digestmod=hashlib.sha256).digest()
-        encodesignature = base64.b64encode(signature).decode()
         try:
-            global comment
+            global comment, fas, ppkPelayanan
             comment = request.POST['catatan']
             faskes = request.POST['faskes']
-            global fas
             url = 'https://new-api.bpjs-kesehatan.go.id:8080/new-vclaim-rest/referensi/faskes/%s/2' % faskes 
-            headers = {
-                "Accept":"application/json", 
-                "X-cons-id":consID,
-                "X-timestamp":stamp,
-                "X-signature":encodesignature
-            }
-
-            response = requests.get(url,headers = headers)
-            fas = response.json()
-            global ppkPelayanan
+            fas = getApi(url)
+            # print(fas)
             ppkPelayanan = fas['response']['faskes'][0]['kode']
-            print(ppkPelayanan)
-            print(comment)
-            # print(consID)
-            # print(stamp)
-            # print(encodesignature)
+            # print(ppkPelayanan)
+            # print(comment)
         except:
             return "none"
 
 
     elif 'dpjp' in request.POST:
-
-        consID = '27952'
-        # secretKey = 'rsm32h1'
-        stamp = str(
-                round(
-                    time.time()
-                )
-                )
-        data = consID + '&' + stamp
-        resultdata = data.encode("utf-8")
-        signature = hmac.new(b"rsm32h1", resultdata, digestmod=hashlib.sha256).digest()
-        encodesignature = base64.b64encode(signature).decode()
         try:
-            url = 'https://new-api.bpjs-kesehatan.go.id:8080/new-vclaim-rest/referensi/dokter/pelayanan/'+ pelayanan + '/tglPelayanan/'+ dateNow + '/Spesialis/' + kodeSpesialisRujukan
-            headers = {
-                "Accept":"application/json", 
-                "X-cons-id":consID,
-                "X-timestamp":stamp,
-                "X-signature":encodesignature
-            }
-
             global dpjp
-            response = requests.get(url,headers = headers)
-            dpjp = response.json()
-            # dp = fas['response']['faskes'][0]['kode']
-            # print()
-            # print(comment)
-            # print(consID)
-            # print(stamp)
-            # print(encodesignature)
+            url = 'https://new-api.bpjs-kesehatan.go.id:8080/new-vclaim-rest/referensi/dokter/pelayanan/'+ pelayanan + '/tglPelayanan/'+ dateNow + '/Spesialis/' + kodeSpesialisRujukan
+            dpjp = getApi(url)
         except:
             return "none"
 
-
-
     elif request.POST:
-        consID = '27952'
-        # secretKey = 'rsm32h1'
-        stamp = str(
-                round(
-                    time.time()
-                )
-                )
-        data = consID + '&' + stamp
-        resultdata = data.encode("utf-8")
-        signature = hmac.new(b"rsm32h1", resultdata, digestmod=hashlib.sha256).digest()
-        encodesignature = base64.b64encode(signature).decode()
         noKartu = diag['response']['rujukan']['peserta']['noKartu']
         noMR = diag['response']['rujukan']['peserta']['mr']['noMR']
         tglrujukan = diag['response']['rujukan']['tglKunjungan']
@@ -196,12 +107,6 @@ def index(request):
         poliTujuan = diag['response']['rujukan']['poliRujukan']['kode']
         noDpjp = dpjp['response']['list'][0]['kode']
         url = 'https://new-api.bpjs-kesehatan.go.id:8080/new-vclaim-rest/SEP/1.1/insert'
-        headers = {
-            "Accept":"application/json", 
-            "X-cons-id":consID,
-            "X-timestamp":stamp,
-            "X-signature":encodesignature
-        }
         dataKey = json.dumps({
            "request": {
               "t_sep": {
@@ -254,14 +159,12 @@ def index(request):
                  "user": "XX" # null
               }
            }
-        })               
-                                 
-                      
-        response2 = requests.post(url,data = dataKey ,headers = headers) #ngirim ke server BPJS untuk insert SEP
+        })                        
+        # response2 = requests.post(url,data = dataKey ,headers = headers) #ngirim ke server BPJS untuk insert SEP
 
         # clientresponse = requests.post('api server client', data = 'data clientnya apa', headers = 'headersnya apa') # ini untuk ke client
         global hasil
-        hasil = response2.json()
+        hasil = postApi(url,dataKey)
         print(hasil)
    
     return render(request,'index.html', {
